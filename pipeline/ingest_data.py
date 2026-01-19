cd5708da70467dfc4e23d73d1b9799911dc2abca
christianel <christiane.lorenz@physics.ox.ac.uk>
2026-01-19 11:53:09 +0100

    Ingest script

---
+++ pipeline/ingest_data.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # New York Taxi Dataset
+
+import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+
+def run():
+
+    pg_user ='root'
+    pg_pass ='root'
+    pg_host ='localhost'
+    pg_port=5432
+    pg_db='ny_taxi'
+
+    year=2021
+    month=1
+
+    # Read a sample of the data
+    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
+    url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz
+    url 
+
+    chunksize=100000
+
+    dtype = {
+        "VendorID": "Int64",
+        "passenger_count": "Int64",
+        "trip_distance": "float64",
+        "RatecodeID": "Int64",
+        "store_and_fwd_flag": "string",
+        "PULocationID": "Int64",
+        "DOLocationID": "Int64",
+        "payment_type": "Int64",
+    "fare_amount": "float64",
+    "extra": "float64",
+    "mta_tax": "float64",
+    "tip_amount": "float64",
+    "tolls_amount": "float64",
+    "improvement_surcharge": "float64",
+    "total_amount": "float64",
+    "congestion_surcharge": "float64"
+    }
+
+    parse_dates = [
+        "tpep_pickup_datetime",
+        "tpep_dropoff_datetime"
+    ]
+
+    # Connect to the SQL database
+    engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
+
+    df = pd.read_csv(
+        url,    
+        dtype=dtype,
+        parse_dates=parse_dates
+    )
+
+    # Create table, but do not insert data yet
+    df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
+
+    df_iter =pd.read_csv(
+        prefix + 'yellow_tripdata_2021-01.csv.gz',
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True, 
+        chunksize=100000,
+    )
+
+    # Complete ingestion loop
+    first = True
+
+    for df_chunk in tqdm(df_iter):
+
+        if first:
+            df_chunk.head(0).to_sql(
+            name="yellow_taxi_data",
+            con=engine,
+            if_exists="replace"
+            )
+            first = False
+            print("Table created")
+
+        # Insert chunk
+        df_chunk.to_sql(
+            name="yellow_taxi_data",
+            con=engine,
+            if_exists="append"
+        )
+
+        print("Inserted:", len(df_chunk))
